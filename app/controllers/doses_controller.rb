7bb9ecb644c6bfa4f5ba0d25bfe9c755b072db43
Delfina Rueda <delfinarueda@gmail.com>
2019-11-14 16:50:20 +0000

    Added controller and view to the app and interlinked everything

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,38 @@
+class DosesController < ApplicationController
+  before_action :set_cocktail, only: [:new, :create]
+
+  def new
+    @dose = Dose.new
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+    respond_to do |format|
+      if @dose.save
+        format.html { redirect_to @cocktail, notice: 'Dose was successfully created.' }
+        format.json { render :show, status: :created, location: @dose }
+      else
+        format.html { render :new }
+        format.json { render json: @dose.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    @cocktail = @dose.cocktail
+    redirect_to cocktail_path(@cocktail)
+  end
+
+  private
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
+end
